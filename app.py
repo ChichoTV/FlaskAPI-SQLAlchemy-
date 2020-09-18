f1ac7fc7d5c2c97462e89cde2318612e873ae0e4
unknown <taylorvought@gmail.com>
2020-09-18 08:46:42 -0700

    final work?

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
+import datetime as dt
 engine = create_engine("sqlite:///hawaii.sqlite")
 Base=automap_base()
 Base.prepare(engine,reflect=True)
 Measure=Base.classes.measurement
 Station=Base.classes.station
-
+latest=dt.date(2017,8,23)
+year_ago=latest-dt.timedelta(days=365)
 
 app = Flask(__name__)
 
@@ -19,6 +21,9 @@ def home():
         f'/api/v1.0/precipitation<br/>'
         f'/api/v1.0/stations<br/>'
         f'/api/v1.0/tobs<br>'
+        f'/api/v1.0/start<br>'
+        f'/api/v1.0/start/end<br>'
+        f'AVAILABLE DATE RANGES: (2010-01-01)-(2017-08-23)'
         )
 
 @app.route("/api/v1.0/precipitation")
@@ -44,6 +49,54 @@ def stations():
         mydict1[num]=stations[num]
     session.close()
     return jsonify(mydict1)
+
+@app.route('/api/v1.0/tobs')
+def tobs():
+    session=Session(engine)
+    temps=session.query(Measure.date,Measure.tobs).filter(Measure.station=="USC00519281",Measure.date>=year_ago).all()
+    dts=[temp[0] for temp in temps]
+    tmp=[temp[1] for temp in temps]
+    mydict={}
+    for num in range(len(dts)):
+        mydict[dts[num]]=tmp[num]
+    return jsonify(mydict)
+    
+@app.route('/api/v1.0/<start>')
+def start_date(start):
+    session=Session(engine)
+    ret=session.query(func.min(Measure.tobs),func.max(Measure.tobs),func.avg(Measure.tobs)).filter(Measure.date>=start).all()
+    temp=[]
+    mydict={}
+    keys=['min','max','avg']
+    for num in range(len(ret[0])):
+        temp.append(ret[0][num])
+    for num in range(len(temp)):
+        mydict[keys[num]]=temp[num]
+    session.close()
+    return jsonify(mydict)
+
+
+
+
+
+@app.route('/api/v1.0/<start>/<end>')
+def start_end(start,end):
+    session=Session(engine)
+    ret=session.query(func.min(Measure.tobs),func.max(Measure.tobs),func.avg(Measure.tobs)).filter(Measure.date>=start,Measure.date<=end).all()
+    temp=[]
+    mydict={}
+    keys=['min','max','avg']
+    for num in range(len(ret[0])):
+        temp.append(ret[0][num])
+    for num in range(len(temp)):
+        mydict[keys[num]]=temp[num]
+    session.close()
+    return jsonify(mydict)
+
+
+
+
+
 
 
 if __name__ == "__main__":
